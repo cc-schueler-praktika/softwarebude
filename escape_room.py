3fce56467f57621cec6c1a1b772bf4393e9bbf2d
Robin <robin.ramspott@codecenric.de>
2019-06-11 15:01:59 +0200

    Fixed bug where starting same thread again causing an exception

--- escape_room.py
+++ escape_room.py
@@ -8,17 +8,18 @@ class EscapeRoom(Room):
     def __init__(self, name, description,  task):
         super().__init__(name, description)
         self.task = task
-        self.count = Countdown(10)              # set time for countdown here
+        self.count = None
 
     def escape_room_intro(self):
-        self.task = Task('Was ist 2+2', '4')  # Question
+        self.task = Task('Was ist 2+2', '4')  #TODO: Set better question
         print('Oh. Hallo. Gut das du da bist. Ich brauche deine Hilfe!')
         helper.wait(2)
         print('Ich komme bei diesem Problem nicht weiter. Kannst du es für mich lösen?')
         helper.wait(2)
         print('Eigentlich hast du keine anderer möglichkeit. Dafür bezahle ich dich schließlich')
         helper.wait(2)
-        self.count.start()                       # start a new thread
+        self.count = Countdown(20)               # sets time for countdown
+        self.count.start()                       # starts a new thread
         self.task.show_question()
         self.start_task()
         self.count.stop_countdown()
